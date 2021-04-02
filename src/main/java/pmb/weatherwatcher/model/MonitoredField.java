3228920f0b3584eda2823b0959510b6c91bf71e9
69pmb <pmbroca@gmail.com>
2021-04-02 16:37:34 +0200

    feat: entities

---
+++ src/main/java/pmb/weatherwatcher/model/MonitoredField.java
@@ -0,0 +1,85 @@
+package pmb.weatherwatcher.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Monitored field database entity.
+ */
+@Entity
+@Table(name = "monitored_field")
+public class MonitoredField {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Field to monitor.
+     */
+    @Enumerated(EnumType.STRING)
+    private WeatherField field;
+
+    /**
+     * Value under this value will trigger an alert.
+     */
+    private Integer min;
+
+    /**
+     * Value above this value will trigger an alert.
+     */
+    private Integer max;
+
+    @JoinColumn(name = "alert", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Alert alert;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public WeatherField getField() {
+        return field;
+    }
+
+    public void setField(WeatherField field) {
+        this.field = field;
+    }
+
+    public Integer getMin() {
+        return min;
+    }
+
+    public void setMin(Integer min) {
+        this.min = min;
+    }
+
+    public Integer getMax() {
+        return max;
+    }
+
+    public void setMax(Integer max) {
+        this.max = max;
+    }
+
+    public Alert getAlert() {
+        return alert;
+    }
+
+    public void setAlert(Alert alert) {
+        this.alert = alert;
+    }
+
+}
---
+++ src/main/java/pmb/weatherwatcher/model/WeatherField.java
@@ -0,0 +1,29 @@
+package pmb.weatherwatcher.model;
+
+/**
+ * Weather fields that can be monitored, the code is the field's name from the Weather API.
+ */
+public enum WeatherField {
+
+    TEMP("tempC"),
+    WIND("windKph"),
+    PRESSURE("pressureMb"),
+    PRECIP("precipMm"),
+    HUMIDITY("humidity"),
+    CLOUD("cloud"),
+    FEELS_LIKE("feelsLikeC"),
+    CHANCE_OF_RAIN("chanceOfRain"),
+    CHANCE_OF_SNOW("chanceOfSnow"),
+    UV("uv");
+
+    private final String code;
+
+    WeatherField(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+}
